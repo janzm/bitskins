481c12871e3a00d8a2baec120a8435852e3f644a
janzm <d.janzm@gmail.com>
2019-12-01 23:44:24 +0800

    fuck bitskins

--- app/src/main/java/com/example/bitskins/MainActivity.java
+++ app/src/main/java/com/example/bitskins/MainActivity.java
@@ -38,7 +38,7 @@ import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
